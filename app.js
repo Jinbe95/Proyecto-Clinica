2d91b75693eea415afb56101e74eb80a7c5ad7ae
Herrera Agustin <agustinherrera451@gmail.com>
2025-05-09 22:04:32 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+document.getElementById(`formContacto`).addEventListener(`submit`, function (event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById(`nombre`).value;
+    const correo = document.getElementById(`correo`).value;
+    const mensaje = document.getElementById(`mensaje`).value;
+
+    swal.fire({
+        icon: `success`,
+        title: `Mensaje Enviado`,
+        text: `Gracias por contactarnos, ${nombre}. Te responderemos pronto.`,
+        confirmButtonColor: `#00ff88`,
+    });
+
+    document.getElementById(`formContacto`).reset();
+});
